e8a3d089f2b29a82f9aa337eff45488fcfa4d722
Vladyslav Voichenko <vladvoychenko@gmail.com>
2024-02-01 18:56:53 +0200

    Buildings element refactor

---
+++ frontend/src/components/Corpus.tsx
@@ -0,0 +1,54 @@
+import React from "react"
+
+import styles from "./corpus.module.css"
+
+export default function Corpus({
+  heading,
+  icon,
+  link,
+}: {
+  heading: string
+  icon?: string
+  link: string
+}) {
+  return (
+    <div className={"col-sm-3 sm-margin-b-2 " + styles.corpus}>
+      <div
+        className={"wow fadeInLeft " + styles.corpusInner}
+        data-wow-duration=".3"
+        data-wow-delay=".3s"
+      >
+        <div className="service" data-height="height">
+          <div className="service-element">
+            <i className="service-icon">
+              {icon == null ? (
+                <svg
+                  className="svg-inline--fa fa-hotel fa-w-18 service-icon"
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="hotel"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 576 512"
+                  data-fa-i2svg=""
+                >
+                  <path
+                    fill="currentColor"
+                    d="M560 64c8.84 0 16-7.16 16-16V16c0-8.84-7.16-16-16-16H16C7.16 0 0 7.16 0 16v32c0 8.84 7.16 16 16 16h15.98v384H16c-8.84 0-16 7.16-16 16v32c0 8.84 7.16 16 16 16h240v-80c0-8.8 7.2-16 16-16h32c8.8 0 16 7.2 16 16v80h240c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16h-16V64h16zm-304 44.8c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8v-38.4zm0 96c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8v-38.4zm-128-96c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8v-38.4zM179.2 256h-38.4c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4c0 6.4-6.4 12.8-12.8 12.8zM192 384c0-53.02 42.98-96 96-96s96 42.98 96 96H192zm256-140.8c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm0-96c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4z"
+                  ></path>
+                </svg>
+              ) : (
+                <img src={icon} />
+              )}
+            </i>
+          </div>
+          <div className="service-info">
+            <h3>{heading}</h3>
+          </div>
+          <a href={link} className="content-wrapper-link"></a>
+        </div>
+      </div>
+    </div>
+  )
+}
