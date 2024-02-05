e4ec3eefe873c562436b5b09b9ba3857276a70eb
Vladyslav Voichenko <vladvoychenko@gmail.com>
2024-02-05 18:40:07 +0200

    Added faculty card into floor page

--- frontend/src/pages/FloorPage.tsx
+++ frontend/src/pages/FloorPage.tsx
@@ -7,10 +7,11 @@ import AirQuality from "../components/AirQuality"
 import AirStatus from "../components/AirStatus"
 import FloorMap from "../components/FloorMap"
 import FloorHeader from "../components/FloorHeader"
+import FacultiesCard from "../components/FacultiesCard"
 
 export default function FloorPage({ id }: { id: number }) {
   const sensorsBlockStyles = {
-    backgroundColor: "red",
+    backgroundColor: "#235352",
   }
 
   return (
@@ -33,11 +34,41 @@ export default function FloorPage({ id }: { id: number }) {
       {/* style="	background: <%-floor.floorColor%>;" */}
       <div className="ui-60" style={sensorsBlockStyles}>
         <div className="container">
-          <div className="row">
-            {/* TODO: Pass data from server (sensor), pass link instean values, so items would change separetely from parent */}
-            <AirQuality co2={1100} aqi={10} />
+          <div className="infoBox">
+            <div className="infoboxContainer">
+              {/* TODO: parse data from server */}
+              <FacultiesCard
+                params={{
+                  icon: "fa-globe",
+                  name: "Факультет біології, географії та екології",
+                  color: "#53A306",
+                  area: "1319.1 м²",
+                  cathedras: "3",
+                  rooms: "24",
+                  numberOfSeats: "418",
+                  bachelors: {
+                    fullTime: 156,
+                    external: 33,
+                  },
+                  masters: {
+                    fullTime: 49,
+                    external: 17,
+                  },
+                  phd: {
+                    fullTime: 7,
+                    external: 3,
+                  },
+                }}
+              />
+            </div>
 
-            <AirStatus temperature={21.1} humidity={50} />
+            {/* TODO: Pass data from server (sensor), pass link instean values, so items would change separetely from parent */}
+
+            <div className="qualityBoxes">
+              <AirQuality co2={1100} aqi={10} />
+
+              <AirStatus temperature={21.1} humidity={50} />
+            </div>
           </div>
         </div>
       </div>
