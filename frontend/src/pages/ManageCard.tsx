40203b65578639e1337a54febbbe6c9ebc3e806f
Vladyslav Voichenko <vladvoychenko@gmail.com>
2024-02-27 18:49:17 +0200

    manage card form

--- frontend/src/pages/ManageCard.tsx
+++ frontend/src/pages/ManageCard.tsx
@@ -1,163 +1,236 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useForm } from "react-hook-form"
+
 import DashLayout from "../components/DashLayout/DashLayout"
 
 import styles from "./EditPages/editPage.module.css"
 
 const API_URL = process.env.REACT_APP_API_URL
 
+const facultiesAPI = API_URL + "/api/faculties/"
 const facultyCardsAPI = API_URL + "/api/facultyCard/"
 
 export default function ManageCard() {
+  const [faculties, setFaculties] = useState([])
+
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      faculty: "",
+      icon: "fa-globe",
+      seats: 0,
+      color: "#000",
+      area: 0,
+      pulpits: 0,
+      rooms: 0,
+      bachelorFull: 0,
+      bachelorPart: 0,
+      masterFull: 0,
+      masterPart: 0,
+      phdFull: 0,
+      phdPart: 0,
+    },
+  })
+
+  useEffect(() => {
+    const fetchFaculties = async () => {
+      try {
+        const response = await fetch(facultiesAPI)
+        const data = await response.json()
+
+        setFaculties(
+          data.map((faculty: any) => {
+            return faculty.name
+          })
+        )
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    fetchFaculties()
+  }, [])
+
+  function tryCreateFacultyCard(data: any) {
+    console.log(data)
+  }
+
   return (
     <DashLayout>
-      {/* <form className={styles.form} onSubmit={handleSubmit(onSubmit)}> */}
-      <ul className={styles?.list}>
-        <li className="dash-board__item">
-          <label htmlFor="number" className="dash-board__label">
-            Назва
-          </label>
-          <textarea
-            className={styles.textArea}
-            //   defaultValue={page?.heading}
-            //   {...register("heading")}
-          />
-        </li>
-        <li className="dash-board__item">
-          <label htmlFor="number" className="dash-board__label">
-            Іконка
-          </label>
-          <input
-            className={styles.textArea}
-            //   defaultValue={page?.buttonLink}
-            //   {...register("buttonLink")}
-          />
-        </li>
-        <li className="dash-board__item">
-          <label htmlFor="number" className="dash-board__label">
-            Колір
-          </label>
-          <input
-            type="color"
-            className={styles.textArea}
-            //   defaultValue={page?.buttonLink}
-            //   {...register("buttonLink")}
-          />
-        </li>
-        <li className="dash-board__item">
-          <label htmlFor="number" className="dash-board__label">
-            Кількість місць
-          </label>
-          <input
-            type="number"
-            className={styles.textArea}
-            //   defaultValue={page?.buttonLink}
-            //   {...register("buttonLink")}
-          />
-        </li>
-        <li className="dash-board__item">
-          <label htmlFor="number" className="dash-board__label">
-            Площа
-          </label>
-          <input
-            type="number"
-            className={styles.textArea}
-            //   defaultValue={page?.buttonLink}
-            //   {...register("buttonLink")}
-          />
-        </li>
-        <li className="dash-board__item">
-          <label htmlFor="number" className="dash-board__label">
-            Кількість кафедр
-          </label>
-          <input
-            type="number"
-            className={styles.textArea}
-            //   defaultValue={page?.buttonLink}
-            //   {...register("buttonLink")}
-          />
-        </li>
-        <li className="dash-board__item">
-          <label htmlFor="number" className="dash-board__label">
-            Кількість приміщень
-          </label>
-          <input
-            type="number"
-            className={styles.textArea}
-            //   defaultValue={page?.buttonLink}
-            //   {...register("buttonLink")}
-          />
-        </li>
-        <li className="dash-board__item">
-          <label htmlFor="number" className="dash-board__label">
-            Бакалаврів (денна форма)
-          </label>
-          <input
-            type="number"
-            className={styles.textArea}
-            //   defaultValue={page?.buttonLink}
-            //   {...register("buttonLink")}
-          />
-        </li>
-        <li className="dash-board__item">
-          <label htmlFor="number" className="dash-board__label">
-            Бакалаврів (заочна форма)
-          </label>
-          <input
-            type="number"
-            className={styles.textArea}
-            //   defaultValue={page?.buttonLink}
-            //   {...register("buttonLink")}
-          />
-        </li>
-        <li className="dash-board__item">
-          <label htmlFor="number" className="dash-board__label">
-            Магістрів (денна форма)
-          </label>
-          <input
-            type="number"
-            className={styles.textArea}
-            //   defaultValue={page?.buttonLink}
-            //   {...register("buttonLink")}
-          />
-        </li>
-        <li className="dash-board__item">
-          <label htmlFor="number" className="dash-board__label">
-            Магістрів (заочна форма)
-          </label>
-          <input
-            type="number"
-            className={styles.textArea}
-            //   defaultValue={page?.buttonLink}
-            //   {...register("buttonLink")}
-          />
-        </li>
-        <li className="dash-board__item">
-          <label htmlFor="number" className="dash-board__label">
-            PHD (денна форма)
-          </label>
-          <input
-            type="number"
-            className={styles.textArea}
-            //   defaultValue={page?.buttonLink}
-            //   {...register("buttonLink")}
-          />
-        </li>
-        <li className="dash-board__item">
-          <label htmlFor="number" className="dash-board__label">
-            PHD (заочна форма)
-          </label>
-          <input
-            type="number"
-            className={styles.textArea}
-            //   defaultValue={page?.buttonLink}
-            //   {...register("buttonLink")}
-          />
-        </li>
-      </ul>
-      <button type="submit" id="save" className="dash-button">
-        Зберегти
-      </button>
-      {/* </form> */}
+      <form
+        className={styles.form}
+        onSubmit={handleSubmit(tryCreateFacultyCard)}
+      >
+        <ul className={styles?.list}>
+          <li className="dash-board__item">
+            <label htmlFor="faculty" className="dash-board__label">
+              Назва
+            </label>
+            <select id="faculty" {...register("faculty")}>
+              <option value="" disabled selected hidden>
+                ---Оберіть факультет---
+              </option>
+              {faculties.map((faculty) => {
+                return <option value={faculty}>{faculty}</option>
+              })}
+            </select>
+          </li>
+          <li className="dash-board__item">
+            {/* ДРОПДАУН АБО ПОСИЛАННЯ НА ФАЙЛ АБО ДОКИ */}
+            <label htmlFor="icon" className="dash-board__label">
+              Іконка (FontAwesome клас)
+            </label>
+            <input
+              id="icon"
+              className={styles.textArea}
+              defaultValue={"fa-globe"}
+              {...register("icon")}
+            />
+          </li>
+          <li className="dash-board__item">
+            <label htmlFor="color" className="dash-board__label">
+              Колір
+            </label>
+            <input
+              id="color"
+              type="color"
+              className={styles.textArea}
+              {...register("color")}
+            />
+          </li>
+          <li className="dash-seats">
+            <label htmlFor="capacity" className="dash-board__label">
+              Кількість місць
+            </label>
+            <input
+              id="seats"
+              type="number"
+              className={styles.textArea}
+              defaultValue={0}
+              min={0}
+              {...register("seats")}
+            />
+          </li>
+          <li className="dash-board__item">
+            <label htmlFor="area" className="dash-board__label">
+              Площа
+            </label>
+            <input
+              id="area"
+              type="number"
+              className={styles.textArea}
+              defaultValue={0}
+              min={0}
+              {...register("area")}
+            />
+          </li>
+          <li className="dash-board__item">
+            <label htmlFor="pulpits" className="dash-board__label">
+              Кількість кафедр
+            </label>
+            <input
+              id="pulpits"
+              type="number"
+              className={styles.textArea}
+              defaultValue={0}
+              min={0}
+              {...register("pulpits")}
+            />
+          </li>
+          <li className="dash-board__item">
+            <label htmlFor="rooms" className="dash-board__label">
+              Кількість приміщень
+            </label>
+            <input
+              id="rooms"
+              type="number"
+              className={styles.textArea}
+              defaultValue={0}
+              min={0}
+              {...register("rooms")}
+            />
+          </li>
+          <li className="dash-board__item">
+            <label htmlFor="bachelorFull" className="dash-board__label">
+              Бакалаврів (денна форма)
+            </label>
+            <input
+              id="bachelorFull"
+              type="number"
+              className={styles.textArea}
+              defaultValue={0}
+              min={0}
+              {...register("bachelorFull")}
+            />
+          </li>
+          <li className="dash-board__item">
+            <label htmlFor="bachelorPart" className="dash-board__label">
+              Бакалаврів (заочна форма)
+            </label>
+            <input
+              id="bachelorPart"
+              type="number"
+              className={styles.textArea}
+              defaultValue={0}
+              min={0}
+              {...register("bachelorPart")}
+            />
+          </li>
+          <li className="dash-board__item">
+            <label htmlFor="masterFull" className="dash-board__label">
+              Магістрів (денна форма)
+            </label>
+            <input
+              id="masterFull"
+              type="number"
+              className={styles.textArea}
+              defaultValue={0}
+              min={0}
+              {...register("masterFull")}
+            />
+          </li>
+          <li className="dash-board__item">
+            <label htmlFor="masterPart" className="dash-board__label">
+              Магістрів (заочна форма)
+            </label>
+            <input
+              id="masterPart"
+              type="number"
+              className={styles.textArea}
+              defaultValue={0}
+              min={0}
+              {...register("masterPart")}
+            />
+          </li>
+          <li className="dash-board__item">
+            <label htmlFor="phdFull" className="dash-board__label">
+              PHD (денна форма)
+            </label>
+            <input
+              id="phdFull"
+              type="number"
+              className={styles.textArea}
+              defaultValue={0}
+              min={0}
+              {...register("phdFull")}
+            />
+          </li>
+          <li className="dash-board__item">
+            <label htmlFor="phdPart" className="dash-board__label">
+              PHD (заочна форма)
+            </label>
+            <input
+              id="phdPart"
+              type="number"
+              className={styles.textArea}
+              defaultValue={0}
+              min={0}
+              {...register("phdPart")}
+            />
+          </li>
+        </ul>
+        <button type="submit" id="save" className="dash-button">
+          Зберегти
+        </button>
+      </form>
     </DashLayout>
   )
 }
