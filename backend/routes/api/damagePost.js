310f17d6efa69c336be663602915884a6092b44a
Vladyslav Voichenko <vladvoychenko@gmail.com>
2024-03-04 20:48:29 +0200

    update operation

--- backend/routes/api/damagePost.js
+++ backend/routes/api/damagePost.js
@@ -127,98 +127,67 @@ router.post("/", requireAuth, upload.any("images"), async (req, res) => {
 // })
 
 // //update one
-// router.patch(
-//   "/:number",
-//   requireAuth,
-//   upload.any("images"),
-//   getRoom,
-//   async (req, res) => {
-//     let isFacultyExists = true
-//     let isFloorExists = true
+router.patch(
+  "/:name",
+  requireAuth,
+  upload.any("images"),
+  getDamage,
+  async (req, res) => {
+    let isBuildingExists = await Building.exists({ name: req.body.building })
 
-//     if (req.body.floor) req.body.floor = Number(req.body.floor)
-//     if (req.body.pulpits) req.body.pulpits = JSON.parse(req.body.pulpits[0])
+    if (req.body.building != [] && req.body.faculty) {
+      isBuildingExists = await Building.exists({ name: req.body.building })
 
-//     if (req.body.floor != [] && req.body.floor) {
-//       isFloorExists = await Floor.exists({ number: req.body.floor })
-//     }
-//     if (req.body.faculty != [] && req.body.faculty) {
-//       isFacultyExists = await Faculty.exists({ name: req.body.faculty })
-//     }
+      if (req.body.sum) {
+        req.body.sum = Number(req.body.sum)
+      }
+    }
 
-//     let pulpitsArray = []
+    let images = []
 
-//     let isPulpitssExists = true
+    if (req.files) {
+      images = req.files.map((file) => file.filename)
+    }
 
-//     const images = req.files.map((file) => file.filename)
+    res.damage.photo_links.forEach((link) => {
+      const address = path.resolve("./static/images/" + link)
+      if (fs.existsSync(address)) {
+        fs.unlinkSync(address)
+      }
+    })
+    if (isBuildingExists) {
+      if (req.body.name != null) {
+        res.damage.name = req.body.name
+      }
+      if (req.body.photo_links != null) {
+        res.damage.photo_links = images
+      }
+      if (req.body.status != null) {
+        res.damage.status = req.body.status
+      }
+      if (req.body.building != null) {
+        res.damage.building = req.body.building
+      }
+      if (req.body.location != null) {
+        res.damage.location = req.body.location
+      }
+      if (req.body.description != null) {
+        res.damage.description = req.body.description
+      }
+      if (req.body.sum != null) {
+        res.damage.sum = req.body.sum
+      }
 
-//     res.room.photo_links.forEach((link) => {
-//       const address = path.resolve("./static/images/" + link)
-//       if (fs.existsSync(address)) {
-//         fs.unlinkSync(address)
-//       }
-//     })
-
-//     if (req.body.pulpits) {
-//       for (const pulpit of req.body.pulpits) {
-//         pulpitsArray.push(null != (await Pulpit.exists({ name: pulpit })))
-//       }
-//       isPulpitssExists = pulpitsArray.every((i) => i === true)
-//     }
-
-//     if (isFacultyExists && isFloorExists && isPulpitssExists) {
-//       if (req.body.number != null) {
-//         res.room.number = req.body.number
-//       }
-//       if (req.body.floor != null) {
-//         res.room.floor = req.body.floor
-//       }
-//       if (req.body.faculty != null) {
-//         res.room.faculty = req.body.faculty
-//       }
-//       if (req.body.capacity != null) {
-//         res.room.capacity = req.body.capacity
-//       }
-//       if (req.body.type != null) {
-//         res.room.type = req.body.type
-//       }
-//       if (req.body.photo_links != null) {
-//         res.room.photo_links = images
-//       }
-//       if (req.body.description != null) {
-//         res.room.description = req.body.description
-//       }
-//       if (req.body.assistant != null) {
-//         res.room.assistant = req.body.assistant
-//       }
-//       if (req.body.model != null) {
-//         res.room.model = req.body.model
-//       }
-//       if (req.body.pulpits != null) {
-//         res.room.pulpits = req.body.pulpits
-//       }
-//       if (req.body.co2 != null) {
-//         res.room.co2 = req.body.co2
-//       }
-//       if (req.body.temperature != null) {
-//         res.room.temperature = req.body.temperature
-//       }
-//       if (req.body.co2_history != null) {
-//         res.room.co2_history = req.body.co2_history
-//       }
-//       if (req.body.temperature_history != null) {
-//         res.room.temperature_history = req.body.temperature_history
-//       }
-//       try {
-//         const updatedRoom = await res.room.save()
-//         res.json(updatedRoom)
-//       } catch (err) {
-//         res.status(400).json({ message: err.message })
-//       }
-//     } else {
-//       res.status(400).json({ message: "Invalid floor, faculty or pulpit" })
-//     }
-//   }
-// )
+      try {
+        const updatedBuilding = await res.damage.save()
+        res.json(updatedBuilding)
+      } catch (err) {
+        res.status(400).json({ message: err.message })
+      }
+    } else {
+      res.status(400).json({ message: "Invalid building" })
+    }
+  }
+)
 
 module.exports = router
