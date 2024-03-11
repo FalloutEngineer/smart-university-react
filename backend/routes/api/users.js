c1f684111a389881a4710c6bff191ac4feceabba
Vladyslav Voichenko <vladvoychenko@gmail.com>
2024-03-11 20:32:15 +0200

    minor change

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,23 +4,6 @@ const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
 const User = require("../../models/user.js")
-const { secret } = require("../../config.js")
-
-router.post("/register", async (req, res) => {
-  try {
-    const { login, password } = req.body
-    const candidate = await User.findOne({ login })
-    if (candidate) {
-      return res.status(400).json({ message: "Такий користувач вже існує" })
-    }
-    const hashedPassword = bcrypt.hashSync(password, 7)
-    const user = new User({ login, password: hashedPassword })
-    await user.save()
-    return res.json({ message: `User ${login} was created` })
-  } catch (err) {
-    res.status(400).json({ message: err.message })
-  }
-})
 
 //get all
 router.get("/", requireAuth, async (req, res) => {
