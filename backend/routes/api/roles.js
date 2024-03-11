27eff34cc772e5840d8ad405c5a7dc42547364ac
Vladyslav Voichenko <vladvoychenko@gmail.com>
2024-03-11 20:41:21 +0200

    minor change

--- backend/routes/api/roles.js
+++ backend/routes/api/roles.js
@@ -86,16 +86,6 @@ router.delete("/:name", requireAuth, getRole, async (req, res) => {
   }
 })
 
-//   name: { type: String, unique: true, required: true },
-//   isSuperAdmin: { type: Boolean, unique: false, required: true },
-//   isAdmin: { type: Boolean, unique: true, required: true },
-//   isEditor: { type: Boolean, unique: false, required: true },
-//   canEditDamage: { type: Boolean, unique: false, required: true },
-//   buildings: [{ type: ObjectId, ref: "Building" }],
-//   floors: [[{ type: ObjectId, ref: "Floor" }]],
-//   faculties: [{ type: ObjectId, ref: "Faculty" }],
-//   rooms: [{ type: ObjectId, ref: "Room" }],
-
 //edit one
 //TODO: REQUIRE SUPERADMIN RIGHTS
 router.patch("/:name", requireAuth, getRole, async (req, res) => {
