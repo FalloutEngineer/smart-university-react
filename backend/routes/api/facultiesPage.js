bc3c6c86456438d1ed8d83762af3db4c982393de
Vladyslav Voichenko <vladvoychenko@gmail.com>
2024-03-15 18:35:56 +0200

    faculties page backend restriction

--- backend/routes/api/facultiesPage.js
+++ backend/routes/api/facultiesPage.js
@@ -7,6 +7,7 @@ const fs = require("fs")
 const FacultiesPage = require("../../models/facultiesPage")
 
 const requireAuth = require("../../middleware/requireAuth.js")
+const { isEditor } = require("../../util/permissionsCheckers.js")
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -33,27 +34,31 @@ router.get("/", async (req, res) => {
 
 //create one
 router.post("/", requireAuth, async (req, res) => {
-  const isFacultiesPagesExists = await FacultiesPage.exists({})
+  if (isEditor(req.role)) {
+    const isFacultiesPagesExists = await FacultiesPage.exists({})
 
-  if (!isFacultiesPagesExists) {
-    const facultiesPage = new FacultiesPage({
-      heading: req.body.heading,
-      description: req.body.description,
-      images: req.body.images,
-    })
-
-    try {
-      const newFacultiesPage = await facultiesPage.save()
-
-      res.status(201).json({
-        message: `Successfuly created faculties page!`,
-        page: newFacultiesPage,
+    if (!isFacultiesPagesExists) {
+      const facultiesPage = new FacultiesPage({
+        heading: req.body.heading,
+        description: req.body.description,
+        images: req.body.images,
       })
-    } catch (err) {
-      res.status(400).json({ message: err.message })
+
+      try {
+        const newFacultiesPage = await facultiesPage.save()
+
+        res.status(201).json({
+          message: `Successfuly created faculties page!`,
+          page: newFacultiesPage,
+        })
+      } catch (err) {
+        res.status(400).json({ message: err.message })
+      }
+    } else {
+      res.status(400).json({ message: "Something went wrong" })
     }
   } else {
-    res.status(400).json({ message: "Something went wrong" })
+    res.status(500).json({ message: "Not enough rights" })
   }
 })
 
@@ -64,74 +69,78 @@ router.patch(
   upload.any("images"),
   getFaculties,
   async (req, res) => {
-    const isFacultiesPageExists = await FacultiesPage.exists({})
+    if (isEditor(req.role)) {
+      const isFacultiesPageExists = await FacultiesPage.exists({})
 
-    if (isFacultiesPageExists) {
-      const facultiesPages = await FacultiesPage.find()
+      if (isFacultiesPageExists) {
+        const facultiesPages = await FacultiesPage.find()
 
-      if (req.body.heading) {
-        facultiesPages[0].heading = req.body.heading
-      }
-      if (req.body.description) {
-        facultiesPages[0].description = req.body.description
-      }
-      if (req.body.images) {
-        facultiesPages[0].images = req.body.images
-      }
-
-      const images = req.files.map(
-        (file) => "/images/faculties/" + file.filename
-      )
-
-      facultiesPages[0].images.forEach((link) => {
-        const address = path.resolve("./static/images/faculties/" + link)
-        if (fs.existsSync(address)) {
-          fs.unlinkSync(address)
+        if (req.body.heading) {
+          facultiesPages[0].heading = req.body.heading
         }
-      })
+        if (req.body.description) {
+          facultiesPages[0].description = req.body.description
+        }
+        if (req.body.images) {
+          facultiesPages[0].images = req.body.images
+        }
 
-      if (images != null) {
-        facultiesPages[0].images = images
-      }
+        const images = req.files.map(
+          (file) => "/images/faculties/" + file.filename
+        )
 
-      try {
-        const updatedFacultiesPage = await facultiesPages[0].save()
-        res.json(updatedFacultiesPage)
-      } catch (err) {
-        res.status(400).json({ message: err.message })
-      }
-    } else {
-      const isFacultiesPagesExists = await FacultiesPage.exists({})
-
-      if (!isFacultiesPagesExists) {
-        const facultiesPage = new FacultiesPage({
-          heading:
-            req.body.heading && req.body.heading !== ""
-              ? req.body.heading
-              : "Факультети",
-          description:
-            req.body.description && req.body.description !== ""
-              ? req.body.description
-              : "Опис",
-          images:
-            req.body.images && req.body.images.length !== 0
-              ? req.body.images
-              : [],
+        facultiesPages[0].images.forEach((link) => {
+          const address = path.resolve("./static/images/faculties/" + link)
+          if (fs.existsSync(address)) {
+            fs.unlinkSync(address)
+          }
         })
 
-        try {
-          const newFacultiesPage = await facultiesPage.save()
+        if (images != null) {
+          facultiesPages[0].images = images
+        }
 
-          res.status(201).json({
-            message: `Successfuly created faculties page!`,
-            page: newFacultiesPage,
-          })
+        try {
+          const updatedFacultiesPage = await facultiesPages[0].save()
+          res.json(updatedFacultiesPage)
         } catch (err) {
           res.status(400).json({ message: err.message })
         }
       } else {
-        res.status(400).json({ message: "Something went wrong" })
+        const isFacultiesPagesExists = await FacultiesPage.exists({})
+
+        if (!isFacultiesPagesExists) {
+          const facultiesPage = new FacultiesPage({
+            heading:
+              req.body.heading && req.body.heading !== ""
+                ? req.body.heading
+                : "Факультети",
+            description:
+              req.body.description && req.body.description !== ""
+                ? req.body.description
+                : "Опис",
+            images:
+              req.body.images && req.body.images.length !== 0
+                ? req.body.images
+                : [],
+          })
+
+          try {
+            const newFacultiesPage = await facultiesPage.save()
+
+            res.status(201).json({
+              message: `Successfuly created faculties page!`,
+              page: newFacultiesPage,
+            })
+          } catch (err) {
+            res.status(400).json({ message: err.message })
+          }
+        } else {
+          res.status(400).json({ message: "Something went wrong" })
+        }
       }
+    } else {
+      res.status(500).json({ message: "Not enough rights" })
     }
   }
 )
