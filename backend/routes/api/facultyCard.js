daddfe815eca5cd3faab06bdb36ad1313247bee6
Vladyslav Voichenko <vladvoychenko@gmail.com>
2024-03-15 18:34:33 +0200

    faculty card backend restriction

--- backend/routes/api/facultyCard.js
+++ backend/routes/api/facultyCard.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const FacultyCard = require("../../models/facultyCard")
 
 const requireAuth = require("../../middleware/requireAuth.js")
+const { isEditor } = require("../../util/permissionsCheckers.js")
 
 //get all
 router.get("/", async (req, res) => {
@@ -21,101 +22,115 @@ router.get("/:name", getCard, (req, res) => {
 
 //create one
 router.post("/", requireAuth, async (req, res) => {
-  const isFacultyCardExists = await FacultyCard.exists({ name: req.body.name })
-
-  if (!isFacultyCardExists) {
-    const facultyCard = new FacultyCard({
+  if (isEditor(req.role)) {
+    const isFacultyCardExists = await FacultyCard.exists({
       name: req.body.name,
-      icon: req.body.icon,
-      area: req.body.area,
-      color: req.body.color,
-      seats: req.body.seats,
-      pulpits: req.body.pulpits,
-      rooms: req.body.rooms,
-      bachelorFull: req.body.bachelorFull,
-      bachelorPart: req.body.bachelorPart,
-      masterFull: req.body.masterFull,
-      masterPart: req.body.masterPart,
-      phdFull: req.body.phdFull,
-      phdPart: req.body.phdPart,
     })
 
-    try {
-      const newFacultyCard = await facultyCard.save()
-
-      res.status(201).json({
-        message: `Successfuly created faculty card!`,
-        page: newFacultyCard,
+    if (!isFacultyCardExists) {
+      const facultyCard = new FacultyCard({
+        name: req.body.name,
+        icon: req.body.icon,
+        area: req.body.area,
+        color: req.body.color,
+        seats: req.body.seats,
+        pulpits: req.body.pulpits,
+        rooms: req.body.rooms,
+        bachelorFull: req.body.bachelorFull,
+        bachelorPart: req.body.bachelorPart,
+        masterFull: req.body.masterFull,
+        masterPart: req.body.masterPart,
+        phdFull: req.body.phdFull,
+        phdPart: req.body.phdPart,
       })
-    } catch (err) {
-      res.status(400).json({ message: err.message })
+
+      try {
+        const newFacultyCard = await facultyCard.save()
+
+        res.status(201).json({
+          message: `Successfuly created faculty card!`,
+          page: newFacultyCard,
+        })
+      } catch (err) {
+        res.status(400).json({ message: err.message })
+      }
+    } else {
+      res.status(400).json({ message: "Faculty already exists" })
     }
   } else {
-    res.status(400).json({ message: "Faculty already exists" })
+    res.status(500).json({ message: "Not enough rights" })
   }
 })
 
 // edit one
 router.patch("/:name", requireAuth, getCard, async (req, res) => {
-  if (req.body.name != null) {
-    res.card.name = req.body.name
-  }
-  if (req.body.icon != null) {
-    res.card.icon = req.body.icon
-  }
-  if (req.body.area != null) {
-    res.card.area = req.body.area
-  }
-  if (req.body.color != null) {
-    res.card.color = req.body.color
-  }
-  if (req.body.seats != null) {
-    res.card.seats = req.body.seats
-  }
-  if (req.body.pulpits != null) {
-    res.card.pulpits = req.body.pulpits
-  }
-  if (req.body.cards != null) {
-    res.card.cards = req.body.cards
-  }
+  if (isEditor(req.role)) {
+    if (req.body.name != null) {
+      res.card.name = req.body.name
+    }
+    if (req.body.icon != null) {
+      res.card.icon = req.body.icon
+    }
+    if (req.body.area != null) {
+      res.card.area = req.body.area
+    }
+    if (req.body.color != null) {
+      res.card.color = req.body.color
+    }
+    if (req.body.seats != null) {
+      res.card.seats = req.body.seats
+    }
+    if (req.body.pulpits != null) {
+      res.card.pulpits = req.body.pulpits
+    }
+    if (req.body.cards != null) {
+      res.card.cards = req.body.cards
+    }
 
-  if (req.body.rooms != null) {
-    res.card.rooms = req.body.rooms
-  }
+    if (req.body.rooms != null) {
+      res.card.rooms = req.body.rooms
+    }
 
-  if (req.body.bachelorFull != null) {
-    res.card.bachelorFull = req.body.bachelorFull
-  }
-  if (req.body.bachelorPart != null) {
-    res.card.bachelorPart = req.body.bachelorPart
-  }
-  if (req.body.masterFull != null) {
-    res.card.masterFull = req.body.masterFull
-  }
-  if (req.body.masterPart != null) {
-    res.card.masterPart = req.body.masterPart
-  }
-  if (req.body.phdFull != null) {
-    res.card.phdFull = req.body.phdFull
-  }
-  if (req.body.phdPart != null) {
-    res.card.phdPart = req.body.phdPart
-  }
-  try {
-    const updatedCard = await res.card.save()
-    res.json(updatedCard)
-  } catch (err) {
-    res.status(400).json({ message: err.message })
+    if (req.body.bachelorFull != null) {
+      res.card.bachelorFull = req.body.bachelorFull
+    }
+    if (req.body.bachelorPart != null) {
+      res.card.bachelorPart = req.body.bachelorPart
+    }
+    if (req.body.masterFull != null) {
+      res.card.masterFull = req.body.masterFull
+    }
+    if (req.body.masterPart != null) {
+      res.card.masterPart = req.body.masterPart
+    }
+    if (req.body.phdFull != null) {
+      res.card.phdFull = req.body.phdFull
+    }
+    if (req.body.phdPart != null) {
+      res.card.phdPart = req.body.phdPart
+    }
+    try {
+      const updatedCard = await res.card.save()
+      res.json(updatedCard)
+    } catch (err) {
+      res.status(400).json({ message: err.message })
+    }
+  } else {
+    res.status(500).json({ message: "Not enough rights" })
   }
 })
 
 // delete one
 router.delete("/:name", requireAuth, getCard, async (req, res) => {
-  try {
-    await res.card.remove()
-    res.json({ message: "Deleted Card" })
-  } catch (err) {
-    res.status(500).json({ message: err.message })
+  if (isEditor(req.role)) {
+    try {
+      await res.card.remove()
+      res.json({ message: "Deleted Card" })
+    } catch (err) {
+      res.status(500).json({ message: err.message })
+    }
+  } else {
+    res.status(500).json({ message: "Not enough rights" })
   }
 })
 
