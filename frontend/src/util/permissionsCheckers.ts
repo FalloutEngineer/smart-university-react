cb76266af21211a92f818853d8b744f00f9dc6bf
Vladyslav Voichenko <vladvoychenko@gmail.com>
2024-03-15 16:09:48 +0200

    canEditThis functions

--- frontend/src/util/permissionsCheckers.ts
+++ frontend/src/util/permissionsCheckers.ts
@@ -14,4 +14,38 @@ export const canEditDamage = (permissions: any) => {
   return isAdmin(permissions) || permissions?.canEditDamage
 }
 
-// module.exports = { isSuperAdmin, isAdmin, isEditor, canEditDamage }
+// can edit at least one
+
+export const canEditAtLeastOneBuilding = (permissions: any) => {
+  return isEditor(permissions) || permissions?.buildings.length > 0
+}
+
+export const canEditAtLeastOneFloor = (permissions: any) => {
+  return isEditor(permissions) || permissions?.floors.length > 0
+}
+
+export const canEditAtLeastOneFaculty = (permissions: any) => {
+  return isEditor(permissions) || permissions?.faculties.length > 0
+}
+
+export const canEditAtLeastOneRoom = (permissions: any) => {
+  return isEditor(permissions) || permissions?.rooms.length > 0
+}
+
+//can edit this
+
+export const canEditThisBuilding = (building: any, permissions: any) => {
+  return isEditor(permissions) || permissions?.rooms.includes(building)
+}
+
+export const canEditThisFloor = (floor: any, permissions: any) => {
+  return isEditor(permissions) || permissions?.floors.includes(floor)
+}
+
+export const canEditThisFaculty = (faculty: any, permissions: any) => {
+  return isEditor(permissions) || permissions?.faculties.includes(faculty)
+}
+
+export const canEditThisRoom = (room: any, permissions: any) => {
+  return isEditor(permissions) || permissions?.rooms.includes(room)
+}
