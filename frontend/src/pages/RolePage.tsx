556fadf2b362ea2e20900eb2a350e974626e70d6
Vladyslav Voichenko <vladvoychenko@gmail.com>
2024-03-15 12:53:11 +0200

    fully functionable "Role Page"

--- frontend/src/pages/RolePage.tsx
+++ frontend/src/pages/RolePage.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import DashLayout from "../components/DashLayout/DashLayout"
 import { NavLink, useNavigate, useParams } from "react-router-dom"
 import { useAuthContext } from "../hooks/useAuthContext"
@@ -10,6 +10,8 @@ const rolesAPI = API_URL + "/api/roles/"
 export default function RolePage() {
   const { user } = useAuthContext()
   const navigate = useNavigate()
+
+  const [role, setRole]: any = useState(null)
 
   const params = useParams()
 
@@ -35,6 +37,28 @@ export default function RolePage() {
     tryDeleteRole()
   }
 
+  useEffect(() => {
+    const fetchRole = async () => {
+      try {
+        const response = await fetch(rolesAPI + params.name, {
+          method: "GET",
+          headers: {
+            authorization: `Bearer ${user}`,
+          },
+        })
+
+        const data = await response.json()
+
+        console.log(data)
+        setRole(data)
+      } catch (e) {
+        alert(e)
+      }
+    }
+
+    fetchRole()
+  }, [])
+
   return (
     <DashLayout>
       <button
@@ -50,39 +74,55 @@ export default function RolePage() {
       <ul className="dash-board__list">
         <li className="dash-board__item">
           <h3 className="dash-board__label">Назва:</h3>
-          <div className="dash-board__value">Асистент 507</div>
+          <div className="dash-board__value">{role?.name}</div>
         </li>
         <li className="dash-board__item">
           <h3 className="dash-board__label">Суперадмін:</h3>
-          <div className="dash-board__value">Ні</div>
+          <div className="dash-board__value">
+            {role?.isSuperAdmin ? "Так" : "Ні"}
+          </div>
         </li>
         <li className="dash-board__item">
           <h3 className="dash-board__label">Адмін:</h3>
-          <div className="dash-board__value">Ні</div>
+          <div className="dash-board__value">
+            {role?.isAdmin ? "Так" : "Ні"}
+          </div>
         </li>
         <li className="dash-board__item">
           <h3 className="dash-board__label">Редактор:</h3>
-          <div className="dash-board__value">Ні</div>
+          <div className="dash-board__value">
+            {role?.isEditor ? "Так" : "Ні"}
+          </div>
         </li>
         <li className="dash-board__item">
           <h3 className="dash-board__label">Можна редагувати збитки:</h3>
-          <div className="dash-board__value">Ні</div>
+          <div className="dash-board__value">
+            {role?.canEditDamage ? "Так" : "Ні"}
+          </div>
         </li>
         <li className="dash-board__item">
           <h3 className="dash-board__label">Будівлі:</h3>
-          <div className="dash-board__value">-</div>
+          <div className="dash-board__value">
+            {role?.buildings.length > 0 ? role?.buildings : "-"}
+          </div>
         </li>
         <li className="dash-board__item">
           <h3 className="dash-board__label">Поверхи:</h3>
-          <div className="dash-board__value">-</div>
+          <div className="dash-board__value">
+            {role?.floors.length > 0 ? role?.floors : "-"}
+          </div>
         </li>
         <li className="dash-board__item">
           <h3 className="dash-board__label">Факультети:</h3>
-          <div className="dash-board__value">-</div>
+          <div className="dash-board__value">
+            {role?.faculties.length > 0 ? role?.faculties : "-"}
+          </div>
         </li>
         <li className="dash-board__item">
           <h3 className="dash-board__label">Приміщення:</h3>
-          <div className="dash-board__value">507</div>
+          <div className="dash-board__value">
+            {role?.rooms.length > 0 ? role?.rooms : "-"}
+          </div>
         </li>
       </ul>
     </DashLayout>
